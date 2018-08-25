2b02035e9016efcb3024313f7dfbf6301dc8ffb3
skanehira <sho19921005@gmail.com>
2018-08-26 08:41:15 +0900

    - modofy message struct - modify message.txt - modify start_bot.sh

--- bot/message.go
+++ bot/message.go
@@ -6,14 +6,16 @@ var PostMessageURL = "https://slack.com/api/chat.postMessage"
 // Message slack message
 // https://api.slack.com/methods/chat.postMessage
 type Message struct {
-	Channel string `json:"channel"`
-	Text    string `json:"text"`
+	Channel   string `json:"channel"`
+	Text      string `json:"text"`
+	LinkNames bool   `json:"link_names"`
 }
 
 // NewMessage New send message
 func NewMessage(channel, text string) *Message {
 	return &Message{
-		Channel: channel,
-		Text:    text,
+		Channel:   channel,
+		Text:      text,
+		LinkNames: true,
 	}
 }
